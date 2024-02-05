753428f565eefec603b9a220238c7bb099332576
RayAlto <tianjiayu668@gmail.com>
2024-02-05 08:32:20 +0800

    feat(BlockInfo): add I/O statistics

--- include/do_not_sleep/block_info.h
+++ include/do_not_sleep/block_info.h
@@ -1,9 +1,10 @@
 #ifndef DONOTSLEEP_DO_NOT_SLEEP_BLOCK_INFO_H_
 #define DONOTSLEEP_DO_NOT_SLEEP_BLOCK_INFO_H_
 
+#include <cstdint>
 #include <filesystem>
-#include <optional>
 #include <unordered_map>
+#include <utility>
 
 namespace ds {
 
@@ -17,12 +18,26 @@ public:
 
   virtual ~BlockInfo() = default;
 
+  // https://docs.kernel.org/block/stat.html#read-sectors-write-sectors-discard-sectors
+  static constexpr const std::uint64_t SECTOR_SIZE = 512 /* bytes */;
+
   // e.g. `/mnt/usb_disk`
-  static std::optional<BlockInfo> from_mount_path(const std::filesystem::path& mount_path);
+  static BlockInfo from_mount_path(const std::filesystem::path& mount_path);
   // e.g. `/dev/sda1`
-  static std::optional<BlockInfo> from_block_path(const std::filesystem::path& block_path);
+  static BlockInfo from_block_path(const std::filesystem::path& block_path);
   // e.g. `sda1`
-  static std::optional<BlockInfo> from_block_name(const std::filesystem::path& block_name);
+  static BlockInfo from_block_name(const std::filesystem::path& block_name);
+
+  // from the stat file
+  [[nodiscard]] std::uint64_t total_reads() const;
+  // from the stat file
+  [[nodiscard]] std::uint64_t total_writes() const;
+  // diff from last call or nullopt if unchanged
+  std::uint64_t reads_taken();
+  // diff from last call or nullopt if unchanged
+  std::uint64_t writes_taken();
+  // diff from last call or nullopt if unchanged
+  std::pair<std::uint64_t, std::uint64_t> io_taken();
 
 protected:
   static const std::filesystem::path MOUNT_INFO_PATH;
@@ -33,12 +48,18 @@ protected:
   static std::unordered_map<std::filesystem::path, std::filesystem::path> mount_list;
 
   static void update_mount_list(const bool& force = false);
-  static std::optional<std::filesystem::path> find_block_stat(const std::filesystem::path& block_device);
+  static std::filesystem::path find_block_stat(const std::filesystem::path& block_device);
+  // get read sectors and write sectors from stat file, return {-1, -1} on error
+  static std::pair<std::uint64_t, std::uint64_t> get_io_statistics(const std::filesystem::path& stat_file);
 
   std::unordered_map<std::filesystem::path, std::filesystem::path>::const_iterator mount_info_iter;
-  std::optional<std::filesystem::path> stat_file;
+  std::filesystem::path stat_file;
+  std::pair<std::uint64_t, std::uint64_t> last_io;
 
   explicit BlockInfo(std::unordered_map<std::filesystem::path, std::filesystem::path>::const_iterator mount_info);
+
+  // get read sectors and write sectors from stat file, return {-1, -1} on error
+  [[nodiscard]] std::pair<std::uint64_t, std::uint64_t> get_io_statistics() const;
 };
 
 } // namespace ds
--- src/block_info.cc
+++ src/block_info.cc
@@ -2,9 +2,12 @@
 
 #include <filesystem>
 #include <fstream>
-#include <optional>
+#include <stdexcept>
 #include <string>
 #include <unordered_map>
+#include <utility>
+
+#include "do_not_sleep/util.h"
 
 namespace ds {
 
@@ -12,18 +15,27 @@ const std::filesystem::path BlockInfo::MOUNT_INFO_PATH{"/proc/self/mountinfo"};
 const std::filesystem::path BlockInfo::SYS_BLOCK_PATH{"/sys/block"};
 const std::filesystem::path BlockInfo::BLOCK_STAT_NAME{"stat"};
 
-std::optional<BlockInfo> BlockInfo::from_mount_path(const std::filesystem::path& mount_path) {
+/* NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables) */
+std::unordered_map<std::filesystem::path, std::filesystem::path> BlockInfo::mount_list;
+
+BlockInfo BlockInfo::from_mount_path(const std::filesystem::path& mount_path) {
+  if (!std::filesystem::is_directory(mount_path)) {
+    throw std::runtime_error{"mount path `" + mount_path.string() + "` is not a directory"};
+  }
   update_mount_list();
   std::unordered_map<std::filesystem::path, std::filesystem::path>::const_iterator mount_info
     = mount_list.find(mount_path);
   if (mount_info == mount_list.end()) {
     // not found
-    return std::nullopt;
+    throw std::runtime_error{"Could not find mount source of `" + mount_path.string() + "`"};
   }
   return BlockInfo{mount_info};
 }
 
-std::optional<BlockInfo> BlockInfo::from_block_path(const std::filesystem::path& block_path) {
+BlockInfo BlockInfo::from_block_path(const std::filesystem::path& block_path) {
+  if (!std::filesystem::is_directory(block_path)) {
+    throw std::runtime_error{"block path `" + block_path.string() + "` is not a directory"};
+  }
   update_mount_list();
   std::unordered_map<std::filesystem::path, std::filesystem::path>::const_iterator mount_info = mount_list.begin();
   for (; mount_info != mount_list.end(); mount_info++) {
@@ -31,10 +43,10 @@ std::optional<BlockInfo> BlockInfo::from_block_path(const std::filesystem::path&
       return BlockInfo{mount_info};
     }
   }
-  return std::nullopt;
+  throw std::runtime_error{"could not find info about `" + block_path.string() + "` in " + MOUNT_INFO_PATH.string()};
 }
 
-std::optional<BlockInfo> BlockInfo::from_block_name(const std::filesystem::path& block_name) {
+BlockInfo BlockInfo::from_block_name(const std::filesystem::path& block_name) {
   update_mount_list();
   std::unordered_map<std::filesystem::path, std::filesystem::path>::const_iterator mount_info = mount_list.begin();
   for (; mount_info != mount_list.end(); mount_info++) {
@@ -42,7 +54,53 @@ std::optional<BlockInfo> BlockInfo::from_block_name(const std::filesystem::path&
       return BlockInfo{mount_info};
     }
   }
-  return std::nullopt;
+  throw std::runtime_error{"could not find info about `" + block_name.string() + "` in " + MOUNT_INFO_PATH.string()};
+}
+
+[[nodiscard]] std::uint64_t BlockInfo::total_reads() const {
+  return get_io_statistics().first;
+}
+
+[[nodiscard]] std::uint64_t BlockInfo::total_writes() const {
+  return get_io_statistics().second;
+}
+
+std::uint64_t BlockInfo::reads_taken() {
+  std::uint64_t reads = get_io_statistics().first;
+  if (reads == last_io.first) {
+    return 0;
+  }
+  std::uint64_t result = reads - last_io.first;
+  last_io.first = reads;
+  return result;
+}
+
+std::uint64_t BlockInfo::writes_taken() {
+  std::uint64_t writes = get_io_statistics().second;
+  if (writes == last_io.second) {
+    return 0;
+  }
+  std::uint64_t result = writes - last_io.second;
+  last_io.second = writes;
+  return result;
+}
+
+std::pair<std::uint64_t, std::uint64_t> BlockInfo::io_taken() {
+  std::pair<std::uint64_t, std::uint64_t> io = get_io_statistics();
+  std::pair<std::uint64_t, std::uint64_t> result;
+  if (io.first == last_io.first) {
+    result.first = 0;
+  } else {
+    result.first = io.first - last_io.first;
+    last_io.first = io.first;
+  }
+  if (io.second == last_io.second) {
+    result.second = 0;
+  } else {
+    result.second = io.second - last_io.second;
+    last_io.second = io.second;
+  }
+  return result;
 }
 
 void BlockInfo::update_mount_list(const bool& force) {
@@ -66,7 +124,7 @@ void BlockInfo::update_mount_list(const bool& force) {
   mount_info_file.close();
 }
 
-std::optional<std::filesystem::path> BlockInfo::find_block_stat(const std::filesystem::path& block) {
+std::filesystem::path BlockInfo::find_block_stat(const std::filesystem::path& block) {
   const std::string block_name = block.has_parent_path() ? block.filename().string() : block.string();
   std::filesystem::path block_path = SYS_BLOCK_PATH / block_name;
   if (std::filesystem::exists(block_path)) {
@@ -89,7 +147,7 @@ std::optional<std::filesystem::path> BlockInfo::find_block_stat(const std::files
     }
   }
   if (!found) {
-    return std::nullopt;
+    throw std::runtime_error{"could not find `" + block.string() + "` in " + SYS_BLOCK_PATH.string()};
   }
   std::filesystem::path device_stat = disk_path / device_name / BLOCK_STAT_NAME;
   if (std::filesystem::exists(device_stat)) {
@@ -102,12 +160,35 @@ std::optional<std::filesystem::path> BlockInfo::find_block_stat(const std::files
     return disk_stat;
   }
   // fuck
-  return std::nullopt;
+  throw std::runtime_error{"could not find stat file of `" + disk_path.string() + "`"};
+}
+
+std::pair<std::uint64_t, std::uint64_t> BlockInfo::get_io_statistics(const std::filesystem::path& stat_file) {
+  std::ifstream stat(stat_file);
+  if (!stat.good()) {
+    DS_LOGERR << "failed to read stat file " << stat_file << '\n';
+    return {-1, -1};
+  }
+  std::uint64_t reads{0};
+  std::uint64_t writes{0};
+  for (int i = 0; i < 3; i++) {
+    stat >> reads;
+  }
+  for (int i = 0; i < 4; i++) {
+    stat >> writes;
+  }
+  stat.close();
+  return {reads, writes};
 }
 
 BlockInfo::BlockInfo(std::unordered_map<std::filesystem::path, std::filesystem::path>::const_iterator mount_info)
   : mount_info_iter(mount_info) {
-  stat_file = find_block_stat(mount_info_iter->first);
+  stat_file = find_block_stat(mount_info_iter->second);
+  last_io = get_io_statistics();
+}
+
+[[nodiscard]] std::pair<std::uint64_t, std::uint64_t> BlockInfo::get_io_statistics() const {
+  return get_io_statistics(stat_file);
 }
 
 } // namespace ds
